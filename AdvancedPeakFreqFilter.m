02f0228bb5826a90b095bf734aac9e81d4c41e32
fatih-iver <fatih.iver@boun.edu.tr>
2019-05-26 19:03:40 +0300

    implement lowpass filter

--- AdvancedPeakFreqFilter.m
+++ AdvancedPeakFreqFilter.m
@@ -1,5 +1,18 @@
 % read the sound file
 [y,Fs] = audioread("PinkPanther30.wav");
-% play the sound file
-sound(y, Fs);
+
+% get number of peaks after applying lowpass filter
+n1 = numel(findpeaks(lowpass(y,1000,Fs)));
+n2 = numel(findpeaks(lowpass(y,2000,Fs)));
+n3 = numel(findpeaks(lowpass(y,3000,Fs)));
+n4 = numel(findpeaks(lowpass(y,4000,Fs)));
+
+% plot number of peaks versus cut-off frequencies
+plot([1000 2000 3000 4000], [n1 n2 n3 n4]);
+
+% name x axis & y axis accordingly
+xlabel('Cut-Off Frequencies'); 
+ylabel('Number of Peaks'); 
+
+
 
